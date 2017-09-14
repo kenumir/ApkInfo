3a7f09c1395219c2d10388ff14ffea06d5a8c02a
kenumir <kenumir@gmail.com>
2017-09-14 21:49:05 +0200

    Start activity + main + cold start

--- app/src/main/java/com/wt/apkinfo/StartActivity.java
+++ app/src/main/java/com/wt/apkinfo/StartActivity.java
@@ -1,5 +1,8 @@
 package com.wt.apkinfo;
 
+import android.content.Intent;
+import android.os.Handler;
+import android.os.Looper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +12,18 @@ public class StartActivity extends AppCompatActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_start);
+	}
+
+	@Override
+	protected void onPostResume() {
+		super.onPostResume();
+		new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+			@Override
+			public void run() {
+				startActivity(new Intent(getApplicationContext(), MainActivity.class));
+				overridePendingTransition(0, 0);
+				finish();
+			}
+		}, 1000);
 	}
 }
