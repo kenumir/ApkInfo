f98305692ba1d4b4b9c5eb0afdad3a726e15c49f
kenumir <kenumir@gmail.com>
2017-09-20 07:02:43 +0200

    Application details activity

--- app/src/main/java/com/wt/apkinfo/activity/ApplicationDetailsActivity.java
+++ app/src/main/java/com/wt/apkinfo/activity/ApplicationDetailsActivity.java
@@ -6,11 +6,18 @@ import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.AppBarLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.AlphaAnimation;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.perf.metrics.AddTrace;
 import com.hivedi.console.Console;
@@ -34,6 +41,7 @@ public class ApplicationDetailsActivity extends AppCompatActivity {
 	public static final String KEY_APP_ID = "application_id";
 
 	@BindView(R2.id.toolbar) Toolbar toolbar;
+	@BindView(R2.id.recycler) RecyclerView recycler;
 	@BindView(R2.id.text1) TextView text1;
 	@BindView(R2.id.text2) TextView text2;
 	@BindView(R2.id.icon1) ImageView icon1;
@@ -53,6 +61,7 @@ public class ApplicationDetailsActivity extends AppCompatActivity {
 
 		appId = getIntent().getStringExtra(KEY_APP_ID);
 
+
 		ApplicationDetailsViewModel.Factory factory = new ApplicationDetailsViewModel.Factory(getApplication(), appId);
 		final ApplicationDetailsViewModel model = ViewModelProviders.of(this, factory).get(ApplicationDetailsViewModel.class);
 		model.getApplicationDetails().observe(this, new Observer<ApplicationDetailsEntity>() {
@@ -60,16 +69,45 @@ public class ApplicationDetailsActivity extends AppCompatActivity {
 			public void onChanged(@Nullable ApplicationDetailsEntity productEntity) {
 				model.setProduct(productEntity);
 				Console.loge("productEntity=" + productEntity);
+				if (productEntity != null) {
+					text1.setText(productEntity.getName());
+					text2.setText(productEntity.getId());
+					icon1.setImageDrawable(productEntity.getIcon());
+				} else {
+					Toast.makeText(getApplicationContext(), "Error load application info", Toast.LENGTH_LONG).show();
+					finish();
+				}
 			}
 		});
 
-		toolbar.setNavigationIcon(R.drawable.ic_arrow_back_white_24dp);
-		toolbar.setNavigationOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View view) {
-				finish();
-			}
-		});
+		//toolbar.setNavigationIcon(R.drawable.ic_arrow_back_white_24dp);
+		//toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+		//	@Override
+		//	public void onClick(View view) {
+		//		finish();
+		//	}
+		//});
 
+		recycler.setLayoutManager(new LinearLayoutManager(this));
+		recycler.setAdapter(new TestAdapter());
+
+	}
+
+	private static class TestAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+		@Override
+		public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+			return null;
+		}
+
+		@Override
+		public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+
+		}
+
+		@Override
+		public int getItemCount() {
+			return 0;
+		}
 	}
 }
